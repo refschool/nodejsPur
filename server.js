ba00e50a6d7d2bc3c6f550597daf66732aff206f
Yvon Huynh <yvon.huynh@gmail.com>
2023-04-28 12:55:00 +0200

    init node project without Express

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    res.end('hello')
+})
+
+server.listen("3000", () => {
+    console.log("Server on 3000 listening...")
+})
