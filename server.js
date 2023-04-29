585b937fa0f21ece84cf9ac41d00f31d01ecfa2b
Yvon Huynh <yvon.huynh@gmail.com>
2023-04-29 17:48:45 +0200

    added route behaviour

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const http = require('http')
+const url = require('url')
 
 const server = http.createServer((req, res) => {
-    res.end('hello')
+
+    if (req.method === 'GET') {
+
+        const requrl = url.parse(req.url, true)
+        const tab = requrl.pathname.split('/')
+        console.log(tab)
+        if (tab[1] === 'project') {
+
+            console.log(requrl.pathname)
+            console.log(requrl.query)
+            res.setHeader('Content-type', 'text/html')
+            res.end('Project id= ' + JSON.stringify(tab[2]))
+
+        }
+    }
+
 })
+
 
 server.listen("3000", () => {
     console.log("Server on 3000 listening...")
